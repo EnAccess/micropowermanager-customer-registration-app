63c1a89af70946c4d1d8919d84fd7aca1cba0a94
Daniel Mohns <daniel.mohns@posteo.de>
2025-05-12 14:21:30 +0200

    Migrate project `build.gradl` to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id 'com.android.application' version '4.2.2' apply false
+    id("com.android.application") version "4.2.2" apply false
 
-    id 'org.jetbrains.kotlin.android' version '1.4.32' apply false
+    id("org.jetbrains.kotlin.android") version "1.4.32" apply false
 }
 
-ext {
-    kotlin_version = '1.4.32'
-    roomVersion = '1.1.1'
-    archLifecycleVersion = '1.1.1'
-}
+extra["kotlin_version"] = "1.4.32"
+extra["roomVersion"] = "1.1.1"
+extra["archLifecycleVersion"] = "1.1.1"
 
-tasks.register('clean', Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
