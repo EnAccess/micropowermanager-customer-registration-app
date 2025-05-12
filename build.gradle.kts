a7ef1fb39d53ed92801ea38c5cd387e5226bf2a8
Daniel Mohns <daniel.mohns@posteo.de>
2025-05-12 14:43:15 +0200

    Add some comments
    based on: https://developer.android.com/build#build-files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+
+    /**
+     * Use `apply false` in the top-level build.gradle file to add a Gradle
+     * plugin as a build dependency but not apply it to the current (root)
+     * project. Don't use `apply false` in sub-projects. For more information,
+     * see Applying external plugins with same version to subprojects.
+     */
+
     id("com.android.application") version "4.2.2" apply false
 
     id("org.jetbrains.kotlin.android") version "1.4.32" apply false
